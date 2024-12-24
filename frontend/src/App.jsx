180203f802f653641bda82cb1d1375e4763ea52f
ryandioneda <er.dioneda@gmail.com>
2024-12-24 13:38:07 -0500

    Added base frontend structure
    Set up frontend structure:
    - Cleaned React + Vite code for clean template
    - Configured Vite for sass

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from './components/Nav/Nav';
+
+import './App.scss';
+function App() {
+
+  return (
+    <Nav />
+
+  )
+}
+
+export default App;
---
+++ frontend/src/components/Nav/Nav.jsx
@@ -0,0 +1,24 @@
+import './Nav.scss';
+
+const Nav = () => {
+    return (
+        <nav className="navbar-wrapper">
+
+            <ul className="navbar-ul">
+
+                <li className="navbar-li">
+                    <a href="" className="nav-link">Home</a>
+                </li>
+                <li className="navbar-li">
+                    <a href="" className="nav-link">About</a>
+                </li>
+                <li className="navbar-li">
+                    <a href="" className="nav-link">Contact</a>
+                </li>
+                
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
