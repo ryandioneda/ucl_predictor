1701051e7427f9bfa0524f14df6174bf1d8342a2
ryandioneda <er.dioneda@gmail.com>
2025-01-24 19:14:57 -0500

    Created RandomForestClassifier
    Changes:
    - Created random forest and trained model
    - Measured accuracy of model

--- backend/model/predict.py
+++ backend/model/predict.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+
 from backend.util.parsers.historical_parsers import parse_ucl_2016_2022_matches_df
 
 ucl_2016_2022_teams_df = pd.read_csv(
@@ -28,5 +31,40 @@ parsed_matches_df = parse_ucl_2016_2022_matches_df(ucl_2016_2022_matches_df)
 parsed_matches_df['team'] = parsed_matches_df['team'].map(lambda team: teams_dict[team]['team_code'])
 parsed_matches_df['opponent'] = parsed_matches_df['opponent'].map(lambda opponent: teams_dict[opponent]['team_code'])
 
+le = LabelEncoder()
 
-print(parsed_matches_df)
+# 2 = win, 1 = lose, 0 = draw
+le = LabelEncoder()
+parsed_matches_df['result'] = le.fit_transform(parsed_matches_df['result'])  # Ensuring the result column is encoded to 2=win, 1=lose, 0=draw
+
+# Create target column: 1 for win (2), 0 for not win (1 or 0)
+parsed_matches_df['target'] = (parsed_matches_df['result'] == 2).astype('int')
+
+import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
+
+rf = RandomForestClassifier(n_estimators=100, min_samples_split=40, random_state=1)
+train = parsed_matches_df[parsed_matches_df["SEASON"] < '2019-2020']
+test = parsed_matches_df[parsed_matches_df["SEASON"] > '2019-2020']
+predictors = ["team", "opponent", "is_home", "hour", "day"]
+rf.fit(train[predictors], train["target"])
+preds = rf.predict(test[predictors])
+
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(test["target"], preds)
+print(acc)
+combined = pd.DataFrame(dict(actual=test["target"], prediction=preds))
+pd.crosstab(index=combined["actual"], columns=combined["prediction"])
+
+from sklearn.metrics import precision_score
+precision_score(test["target"], preds)
+
+# Visualize the first tree in the forest
+plt.figure(figsize=(20,10))
+plot_tree(rf.estimators_[0], filled=True, feature_names=predictors, class_names=["Lose", "Win"], rounded=True)
+plt.savefig('plot.png')
+
+
+
+
